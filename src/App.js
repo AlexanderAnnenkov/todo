ca147cdd16e888aaea7eba27ae63682ddb1ebf83
AlexandrAnnenkov <aleksanderannenkov00@gmail.com>
2021-11-29 12:48:03 +0300

    Add comment in code

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import style from "./App.module.css";
 import Buttons from "./components/buttons/buttons";
 import Items from "./components/items/items";
 import Pagination from "./components/pagination/pagination";
+import {sortBy} from 'lodash'
 
 function App() {
-  const _=require('lodash');
 
-  if (JSON.parse(localStorage.getItem('todos'))===null){
+  if (JSON.parse(localStorage.getItem('todos'))===null){  // Check on empty array  
     localStorage.setItem('todos', JSON.stringify([]))
   }
 
@@ -16,38 +16,38 @@ function App() {
   const [currentPage, setCurrentPage] = useState('0')
   const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')))
 
-  let filterTask = []
-  let allPages =[]
-  let showTasks = []
+  let filterTask = []   // Array for filter task
+  let allPages =[]      // Array with count number page
+  let showTasks = []    // Array for render 5 task in page
   
-
+// 'Switch case' for Filtered task and Sort by date 
   switch(actionType){
     case 'Done' : filterTask=(todos.filter(e => e.isCheck === true))
     break
     case 'Undone': filterTask=(todos.filter(e => e.isCheck === false))
     break
-    case 'Old': filterTask =_.sortBy(todos, 'date')
+    case 'Old': filterTask =sortBy(todos, 'date')
     break
-    case 'New': filterTask =_.sortBy(todos, 'date').reverse()
+    case 'New': filterTask =sortBy(todos, 'date').reverse()
     break
     default: filterTask = todos
     break
   }
-  
+  // const for count page
   const countPages = Math.ceil(filterTask.length / 5);
-
+// Cycle for push in array 'allPages' quantity page
     for (let i = 0 ; i < countPages; i++){
       allPages.push(i)
   }
 
   showTasks = filterTask.slice(currentPage*5 , (currentPage+1)*5)
 
-  
+// onChange event for change value tag 'Input'
   const onNewTextTask = (e) => {
     setText(e.target.value);
     
   }
-
+//onKeyDown event for send object task in array on localstorage
   const sendTask=(event)=>{
     if(event.key === "Enter" && event.target.value !==''){
       todos.unshift({
@@ -60,7 +60,7 @@ function App() {
       setText('')
     }
   }
-  
+// Render components  
   return (
     <div className={style.app}>
       <h1 className={style.title}>ToDo List</h1>
@@ -75,6 +75,7 @@ function App() {
       setActionType={setActionType} 
       actionType={actionType}/>
 
+{/* Component 'Items' is tasks 'Todo list' */}
       <Items 
       state={todos} 
       filterTask={filterTask} 
--- src/components/items/items.jsx
+++ src/components/items/items.jsx
@@ -4,27 +4,27 @@ import style from './items.module.css'
 
 
 let Items = ({state , setTodos, showTasks}) => {    
-    
+// Function 'Delete task'    
     const delItem = (id) =>{
       let newTodos = state.filter((e) => e.id !== id);
       setTodos(newTodos);
       localStorage.setItem("todos", JSON.stringify(newTodos));
     }
-
+//Funcion 'Edit check'
     const switchCheck = (e) => { 
         e.isCheck=!e.isCheck
         localStorage.setItem('todos', JSON.stringify(state))
         setTodos(JSON.parse(localStorage.getItem('todos'))) 
     }
-
+//onDblClick event for switch attribute 'contentEditable' on tag 'span'
     const enableContentEditable = (e) =>{
         e.target.contentEditable=true;
     }
-
+//onBlur event for switch attribute 'contentEditable' on tag 'span' when click on body site
     const disableBlur =(e) =>{
         e.target.contentEditable=false;
     }
-
+//onKeyDown event for accept edit in 'Editmode'
     const editTask = (e, content) =>{
         if (e.key === 'Enter'){
             if(e.target.textContent !==''){
