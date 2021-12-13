00edf87a19a092fbbcd7e2644b81b9538e7df656
AlexandrAnnenkov <aleksanderannenkov00@gmail.com>
2021-12-13 11:29:04 +0300

    Add redirect someone link in login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Login from './components/startPage/startPage'
 import MainContent from './components/content/mainContent'
 import Registration from './components/startPage/registration/registration'
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import {BrowserRouter, Route, Routes,Navigate } from "react-router-dom"
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
     <Route path='/login' element={<Login/>}/>
     <Route path='/main' element={<MainContent/>}/>
     <Route path='/registration' element={<Registration/>}/>
+    <Route path='*' element={<Navigate replace to='/login'/>}/>
   </Routes>
   </BrowserRouter>
   )
