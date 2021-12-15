905f31cd63c53fd8f6ee75c0ab2b6f3ff51ff9d6
AlexanderAnnenkov <aleksanderannenkov00@gmail.com>
2021-12-15 18:47:27 +0300

    Add "drag and drop"

--- src/components/items/items.jsx
+++ src/components/items/items.jsx
@@ -1,6 +1,8 @@
-import axios from "axios"
-import React from "react"
-import style from "./items.module.css"
+import axios from "axios";
+import React , {useState} from "react";
+import style from "./items.module.css";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
 
 let Items = ({
   state,
@@ -10,8 +12,9 @@ let Items = ({
   setTriggerError,
   setAlert,
 }) => {
+  const [updatePos, setUpdatePos] = useState()
 
-  const token = localStorage.getItem("accessToken")
+  const token = localStorage.getItem("accessToken");
 
   // Function 'Delete task'
   const delItem = (id) => {
@@ -22,15 +25,14 @@ let Items = ({
           "Access-Control-Allow-Origin": "*",
           "Content-Type": "application/json;charset=utf-8",
         },
-      })
-      let newTodos = state.filter((e) => e.uuid !== id)
-      setTodos(newTodos)
-
+      });
+      let newTodos = state.filter((e) => e.uuid !== id);
+      setTodos(newTodos);
     } catch (err) {
-      setAlert(err.response.data.message)
-      setTriggerError(true)
+      setAlert(err.response.data.message);
+      setTriggerError(true);
     }
-  }
+  };
 
   //Funcion 'Edit check'
   const switchCheck = async (e) => {
@@ -48,32 +50,32 @@ let Items = ({
             "Content-Type": "application/json;charset=utf-8",
           },
         }
-      )     
-      getTasks()
+      );
+      getTasks();
     } catch (err) {
-      setAlert(err.response.data.message)
-      setTriggerError(true)
+      setAlert(err.response.data.message);
+      setTriggerError(true);
     }
-  }
+  };
 
   //onDblClick event for switch attribute 'contentEditable' on tag 'span'
   const enableContentEditable = (e) => {
-    e.target.contentEditable = true
-  }
+    e.target.contentEditable = true;
+  };
 
   //onBlur event for switch attribute 'contentEditable' on tag 'span' when click on body site
   const disableBlur = (e, content) => {
-    e.target.contentEditable = false
-    e.target.textContent = content.name
-  }
+    e.target.contentEditable = false;
+    e.target.textContent = content.name;
+  };
 
   //onKeyDown event for accept edit in 'Editmode'
   const editTask = async (e, content) => {
     try {
       if (e.key === "Enter") {
         if (e.target.textContent !== "") {
-          const editTask = e.target.textContent
-          content.name = e.target.textContent
+          const editTask = e.target.textContent;
+          content.name = e.target.textContent;
           await axios
             .patch(
               `https://heroku-backend-app-for-todo.herokuapp.com/task/${content.uuid}`,
@@ -90,61 +92,83 @@ let Items = ({
               }
             )
             .then((res) => {
-              e.target.contentEditable = false
-              getTasks()
-            })
+              e.target.contentEditable = false;
+              getTasks();
+            });
         } else {
-          e.target.textContent = content.name
-          e.target.contentEditable = false
+          e.target.textContent = content.name;
+          e.target.contentEditable = false;
         }
       }
       if (e.key === "Escape") {
-        e.target.textContent = content.name
-        e.target.contentEditable = false
+        e.target.textContent = content.name;
+        e.target.contentEditable = false;
       }
     } catch (err) {
-      setAlert(err.response.data.message)
-      setTriggerError(true)
+      setAlert(err.response.data.message);
+      setTriggerError(true);
     }
-  }
+  };
 
   return (
-    <ul className={style.items}>
-      {showTasks.map((t) => (
-        <li key={t.uuid} id={t.uuid} className={style.item}>
-          <input
-            type="checkbox"
-            checked={t.done}
-            onChange={() => {
-              switchCheck(t)
-            }}
-          />
-
-          <span
-            onDoubleClick={enableContentEditable}
-            onKeyDown={(e) => editTask(e, t)}
-            onBlur={(e) => disableBlur(e, t)}
-            className={style.text}
+    <DragDropContext>
+      <Droppable droppableId={style.items}>
+        {(provided) => (
+          <ul
+            className={style.items}
+            {...provided.droppableProps}
+            ref={provided.innerRef}
           >
-            {t.name}
-          </span>
+            {showTasks.map((t, index) => (
+              <Draggable key={t.uuid} draggableId={t.uuid} index={index}>
+                {(provided) => (
+                  <li
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    key={t.uuid}
+                    id={t.uuid}
+                    className={style.item}
+                  >
+                    <input
+                      type="checkbox"
+                      checked={t.done}
+                      onChange={() => {
+                        switchCheck(t);
+                      }}
+                    />
 
-          <span className={style.date}>
-            {new Date(Date.parse(t.createdAt)).toLocaleString()}
-          </span>
+                    <span
+                      onDoubleClick={enableContentEditable}
+                      onKeyDown={(e) => editTask(e, t)}
+                      onBlur={(e) => disableBlur(e, t)}
+                      className={style.text}
+                    >
+                      {t.name}
+                    </span>
 
-          <span
-            onClick={() => {
-              delItem(t.uuid)
-            }}
-            className={style.delete}
-          >
-            <img src="https://cdn-icons-png.flaticon.com/512/2602/2602735.png" />
-          </span>
-        </li>
-      ))}
-    </ul>
-  )
-}
+                    <span className={style.date}>
+                      {new Date(Date.parse(t.createdAt)).toLocaleString()}
+                    </span>
 
-export default Items
+                    <span
+                      onClick={() => {
+                        delItem(t.uuid);
+                      }}
+                      className={style.delete}
+                    >
+                      <img src="https://cdn-icons-png.flaticon.com/512/2602/2602735.png" />
+                    </span>
+                  </li>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </ul>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+};
+
+export default Items;
