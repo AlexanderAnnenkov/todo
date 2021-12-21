81ca573d3e04df9063571f7cf7676415f3b0ca56
AlexandrAnnenkov <aleksanderannenkov00@gmail.com>
2021-12-21 13:03:09 +0300

    Add adminPanel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Login from './components/startPage/startPage'
 import MainContent from './components/content/mainContent'
 import Registration from './components/startPage/registration/registration'
+import AdminPanel from './components/adminPanel/adminPanel'
 import {BrowserRouter, Route, Routes,Navigate } from "react-router-dom"
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Route path='/login' element={<Login/>}/>
     <Route path='/main' element={<MainContent/>}/>
     <Route path='/registration' element={<Registration/>}/>
+    <Route path='/admin' element={<AdminPanel/>}/>
     <Route path='*' element={<Navigate replace to='/login'/>}/>
   </Routes>
   </BrowserRouter>
